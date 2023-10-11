e04a5f5257b6defbe6bef999501ecdbf713d743e
Alisson Youssf <youssfbr@gmail.com>
2023-10-11 11:29:05 -0300

    Autenticacao - Pegando nome e senha POST e separando

--- src/main/java/com/github/youssfbr/todolist/filter/FilterTaskAuth.java
+++ src/main/java/com/github/youssfbr/todolist/filter/FilterTaskAuth.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Base64;
 
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
@@ -14,7 +15,24 @@ public class FilterTaskAuth extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
         // Pegar a autenticacao (usuario e senha)
+        String authorization = request.getHeader("Authorization");
 
+        System.out.println("Authorization");
+        System.out.println(authorization);
+
+        String authEncoded = authorization.substring("Basic".length()).trim();
+        System.out.println(authEncoded);
+
+        byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+        System.out.println(authDecode);
+        String authString = new String(authDecode);
+        System.out.println(authString);
+
+        String[] credentials = authString.split(":");
+        String username = credentials[0];
+        String password = credentials[1];
+        System.out.println(username);
+        System.out.println(password);
         // Validar usuario
 
         // Validar senha
